0859b66df59c450128d395bfbee002a71120f2ec
edi emil <eduardemil10@yahoo.com>
2024-10-25 10:41:18 +0300

    first commit

---
+++ src/main/java/com/dev/ops/integration/DevOpsIntegration/DevOpsIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.dev.ops.integration.DevOpsIntegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevOpsIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevOpsIntegrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dev/ops/integration/DevOpsIntegration/DevOpsIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.ops.integration.DevOpsIntegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevOpsIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
